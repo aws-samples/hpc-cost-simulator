805221df936a7f412fa767546e76feaadc26f60b
Allan Carter <cartalla@amazon.com>
2022-07-25 12:22:21 -0500

    Handle start_time that is 0 or less than submit_time. (#32)
    Add test to LSFLogParser.py to check that jobs.csv is written with the correct,
    non-negative wait_time.
    
    Resolves (#31: JobAnalyzer.py: invalid literal for int() with base 10: '1 day, 6')[https://github.com/aws-samples/hpc-cost-simulator/issues/31]

--- tests/test_LSFLogParser.py
+++ tests/test_LSFLogParser.py
@@ -504,6 +504,26 @@ class TestLSFLogParser:
 
     order += 1
     @pytest.mark.order(order)
+    def test_main_issue_31_no_start_time(self):
+        '''
+        Test for github issue 31
+        '''
+        self.cleanup_files()
+        test_files_dir = 'test_files/LSFLogParser/issues/31'
+        output_dir = 'output/LSFLogParser/issues/31'
+        output_csv = path.join(output_dir, 'jobs.csv')
+        expected_output_csv = path.join(test_files_dir, 'exp_jobs.csv')
+        try:
+            output = check_output([self.lsfLogParser, '--logfile-dir', test_files_dir, '--output-csv', output_csv, '--default-max-mem-gb', str(self.default_max_mem_gb), '-d'], stderr=subprocess.STDOUT, encoding='utf8')
+            print(f"output:\n{output}")
+        except CalledProcessError as e:
+            print(f"return code: {e.returncode}")
+            print(f"output:\n{e.output}")
+            raise
+        assert(filecmp.cmp(output_csv, expected_output_csv, shallow=False))
+
+    order += 1
+    @pytest.mark.order(order)
     def test_main_acct(self):
         self.cleanup_files()
         test_files_dir = 'test_files/LSFLogParser/acct'
