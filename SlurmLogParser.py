c811b7cbdb87f32c8ff19bf822f8b52a76c47d4f
Allan Carter <cartalla@amazon.com>
2022-11-01 20:15:22 -0500

    Handle cancelled slurm job with empty ReqMem field. (#60)
    Handle the case of fields that are empty and are optional.
    
    Resolves #59

--- SlurmLogParser.py
+++ SlurmLogParser.py
@@ -67,35 +67,35 @@ class SlurmLogParser(SchedulerLogParser):
 
     SLURM_ACCT_FIELDS = [
         ('State', 's'),           # Displays the job status, or state. Put this first so can skip ignored states.
-        ('JobID', 's'),       # The identification number of the job or job step
+        ('JobID', 's'),           # The identification number of the job or job step
         # Job properties
-        ('ReqCPUS', 'd'),     # Number of requested CPUs
-        ('ReqMem', 'sd'),     # Minimum required memory for the job in bytes
-        ('ReqNodes', 'd'),    # Requested minimum Node count for the job/step
-        ('Constraints', 's'), #
+        ('ReqCPUS', 'd'),         # Number of requested CPUs
+        ('ReqMem', 'sd', True),   # Minimum required memory for the job in bytes
+        ('ReqNodes', 'd'),        # Requested minimum Node count for the job/step
+        ('Constraints', 's'),     #
         # Times
-        ('Submit', 'dt'),    # The time the job was submitted. NOTE: If a job is requeued, the submit time is reset. This is handled by not overwriting fields with the batch step.
-        ('Eligible', 'dt'),  # When the job became eligible to run
-        ('Start', 'dt'),     # Initiation time of the job
-        ('Elapsed', 'td'),   # The job's elapsed time: [DD-[HH:]]MM:SS
-        ('Suspended', 'td'), # The amount of time a job or job step was suspended
-        ('End', 'dt'),       # Termination time of the job
+        ('Submit', 'dt'),         # The time the job was submitted. NOTE: If a job is requeued, the submit time is reset. This is handled by not overwriting fields with the batch step.
+        ('Eligible', 'dt'),       # When the job became eligible to run
+        ('Start', 'dt'),          # Initiation time of the job
+        ('Elapsed', 'td'),        # The job's elapsed time: [DD-[HH:]]MM:SS
+        ('Suspended', 'td'),      # The amount of time a job or job step was suspended
+        ('End', 'dt'),            # Termination time of the job
 
         ('ExitCode', 's'),        # The exit code returned by the job script or salloc
         ('DerivedExitCode', 's'), # The highest exit code returned by the job's job steps
 
-        ('AllocNodes', 'd'), # Number of nodes allocated to the job/step
-        ('NCPUS', 'd'),      # Total number of CPUs allocated to the job. Equivalent to AllocCPUS
+        ('AllocNodes', 'd'),      # Number of nodes allocated to the job/step
+        ('NCPUS', 'd'),           # Total number of CPUs allocated to the job. Equivalent to AllocCPUS
 
-        ('MaxDiskRead', 'sd'),  # Maximum number of bytes read by all tasks in job')
-        ('MaxDiskWrite', 'sd'), # Maximum number of bytes written by all tasks in job
-        ('MaxPages', 'd'),      # Maximum number of page faults of all tasks in job
-        ('MaxRSS', 'd'),        # Maximum resident set size of all tasks in job
-        ('MaxVMSize', 'd'),     # Maximum Virtual Memory size of all tasks in job
-        ('CPUTime', 'td'),      # Time used (Elapsed time * CPU count) by a job or step in HH:MM:SS format
-        ('UserCPU', 'td'),      # The amount of user CPU time used by the job or job step. Format is the same as Elapsed
-        ('SystemCPU', 'td'),    # The amount of system CPU time used by the job or job step. Format is the same as Elapsed
-        ('TotalCPU', 'td'),     # The sum of the SystemCPU and UserCPU time used by the job or job step
+        ('MaxDiskRead', 'sd', True),  # Maximum number of bytes read by all tasks in job')
+        ('MaxDiskWrite', 'sd', True), # Maximum number of bytes written by all tasks in job
+        ('MaxPages', 'd', True),      # Maximum number of page faults of all tasks in job
+        ('MaxRSS', 'd', True),        # Maximum resident set size of all tasks in job
+        ('MaxVMSize', 'd', True),     # Maximum Virtual Memory size of all tasks in job
+        ('CPUTime', 'td', True),      # Time used (Elapsed time * CPU count) by a job or step in HH:MM:SS format
+        ('UserCPU', 'td', True),      # The amount of user CPU time used by the job or job step. Format is the same as Elapsed
+        ('SystemCPU', 'td', True),    # The amount of system CPU time used by the job or job step. Format is the same as Elapsed
+        ('TotalCPU', 'td', True),     # The sum of the SystemCPU and UserCPU time used by the job or job step
 
         # This field was added, so it should be optional
         ('Partition', 's'),       # The exit code returned by the job script or salloc
@@ -248,6 +248,10 @@ class SlurmLogParser(SchedulerLogParser):
             for field_tuple in self.SLURM_ACCT_FIELDS:
                 field_name = field_tuple[0]
                 field_format = field_tuple[1]
+                if len(field_tuple) == 3:
+                    empty_field_allowed = field_tuple[2]
+                else:
+                    empty_field_allowed = False
                 try:
                     field_value = fields.pop(0)
                 except IndexError:
@@ -256,7 +260,7 @@ class SlurmLogParser(SchedulerLogParser):
                     else:
                         raise
                 req_mem_suffix = None
-                if field_value:
+                if field_value != None:
                     try:
                         if field_format == 'd':
                             field_value = mem_string_to_int(field_value)
@@ -277,11 +281,15 @@ class SlurmLogParser(SchedulerLogParser):
                     except ValueError as e:
                         if field_name == 'Start' and job_fields['State'] == 'CANCELLED':
                             # Ignore cancelled jobs that didn't start
+                            logger.debug(f"Ignoring job because it was CANCELLED and did not start.")
                             return None
-                        field_errors += 1
-                        msg = f"Unable to convert field {field_name} to format {field_format}: {field_value}: {e}\n{line}"
-                        logger.error(f"{self._sacct_input_file}, line {self._line_number}: {msg}")
-                        self.errors.append((self._sacct_input_file, self._line_number, msg))
+                        if empty_field_allowed and field_value == '':
+                            field_value = None
+                        else:
+                            field_errors += 1
+                            msg = f"Unable to convert field {field_name} to format {field_format}: {field_value}: {e}\n{line}"
+                            logger.error(f"{self._sacct_input_file}, line {self._line_number}: {msg}")
+                            self.errors.append((self._sacct_input_file, self._line_number, msg))
                 logger.debug(f"    {field_name}: '{field_value}' {type(field_value)}")
                 job_fields[field_name] = field_value
                 if field_name == 'State':
@@ -313,15 +321,16 @@ class SlurmLogParser(SchedulerLogParser):
         # NCPUS and ReqMem are for the entire job. For a multi-node job this means the per node value must be divided by the number of nodes (AllocNodes).
         if job_fields['AllocNodes'] == 0:
             job_fields['AllocNodes'] = 1
-        if req_mem_suffix:
+        if job_fields['ReqMem'] and req_mem_suffix:
             if req_mem_suffix == 'n':
                 job_fields['ReqMem'] *= job_fields['AllocNodes']
             if req_mem_suffix == 'c' and (job_fields['NCPUS'] > 1):
                 job_fields['ReqMem'] *= job_fields['NCPUS']
 
-        if job_fields['ReqMem'] == 0 and job_fields['MaxRSS']:
-            logger.debug(f"No memory request for job {job_fields['JobID']} so using MaxRSS")
-            job_fields['ReqMem'] = round(job_fields['MaxRSS'] * job_fields['AllocNodes'] * 1.10)
+        # TODO: Add an option to add a ReqMem if one not specified. For now leave it blank or 0.
+        # if job_fields['ReqMem'] == 0 and job_fields['MaxRSS']:
+        #     logger.debug(f"No memory request for job {job_fields['JobID']} so using MaxRSS")
+        #     job_fields['ReqMem'] = round(job_fields['MaxRSS'] * job_fields['AllocNodes'] * 1.10)
 
         return (job_id, job_fields, self._line_number, field_errors)
 
@@ -356,6 +365,7 @@ class SlurmLogParser(SchedulerLogParser):
         logger.debug(f"    Merged job fields:\n{json.dumps(job_fields, indent=4)}")
 
         if field_errors:
+            logger.debug(f"Ignoring job {job_id} because it had {field_errors} field errors.")
             return None
         # Verify that the job has all fields
         missing_fields = []
@@ -397,10 +407,14 @@ class SlurmLogParser(SchedulerLogParser):
         logger.debug(f"_create_job_from_job_fields({job_fields})")
         if job_fields['AllocNodes'] == 0:
             job_fields['AllocNodes'] = 1
+        if job_fields['ReqMem'] != None:
+            max_mem_gb = job_fields['ReqMem'] / MEM_GB
+        else:
+            max_mem_gb = None
         job = SchedulerJobInfo(
             job_id = job_fields['JobID'],
             num_cores = job_fields['NCPUS'],
-            max_mem_gb = job_fields['ReqMem']/MEM_GB,
+            max_mem_gb = max_mem_gb,
             num_hosts = job_fields['AllocNodes'],
 
             submit_time = job_fields['Submit'],
@@ -448,6 +462,8 @@ class SlurmLogParser(SchedulerLogParser):
         Returns:
             (float, str): Tuple with the memory value as bytes and a 'c' or 'n' to indicate if the value is per node or per core.
         '''
+        if not string_value:
+            raise ValueError("Empty string cannot be converted to int")
         if string_value[-1] in ['c', 'n']:
             suffix = string_value[-1]
             string_value = string_value[0:-1]
